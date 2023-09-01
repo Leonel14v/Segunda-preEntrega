64d2d2a1265617c6958fbea4c385c2125b25ea41
Leonel14v <leonelvelazquez902@gmail.com>
2023-09-01 15:53:13 -0300

    pages

--- primera/src/componentes/Navbar/NavBar.js
+++ primera/src/componentes/Navbar/NavBar.js
@@ -6,15 +6,15 @@ import '../Navbar/navbar.css'
 
 function BasicExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">Optica Vision</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Productos</Nav.Link>
-            <Nav.Link href="#link">Conocenos</Nav.Link>
+            <Nav.Link className='links' href="#home">Inicio</Nav.Link>
+            <Nav.Link className='links' href="#link">Productos</Nav.Link>
+            <Nav.Link className='links' href="#link">Conocenos</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
